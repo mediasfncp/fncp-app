2322bd9c0adce5f1748d200aef28882823e13dd4
mediasfncp <medias.fncp@hotmail.com>
2026-03-18 18:23:24 +0100

    Integrate Stripe checkout session creation

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,7 +1,21 @@
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET)
+
 export default async function handler(req,res){
 
- res.json({
-   url:"https://checkout.stripe.com"
+ const session = await stripe.checkout.sessions.create({
+   payment_method_types:["card"],
+   line_items:[
+     {
+       price: process.env.PRICE_1,
+       quantity:1
+     }
+   ],
+   mode:"payment",
+   success_url:"https://fncp-app.vercel.app",
+   cancel_url:"https://fncp-app.vercel.app"
  })
 
+ res.json({url:session.url})
 }
